e68b8374f60e0c8f97bbc2117bacf52fb6240bf1
aintnewtathis-yy <aintnewtathis@mail.ru>
2024-09-13 09:56:21 +0300

    sitemap

---
+++ src/routes/+error.svelte
@@ -0,0 +1,26 @@
+<script>
+    import { page } from '$app/stores';
+</script>
+
+<section>
+    <div class="container">
+        <div class="error-page">
+            <h1>{$page.status}: {$page.error.message}</h1>
+        </div>
+    </div>
+</section>
+
+
+<style lang="scss">
+    .error-page{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        min-height: 70vh;
+
+        h1{
+            font-size: 22px;
+        }
+    }
+</style>
---
+++ src/routes/sitemap.xml/+server.js
@@ -0,0 +1,57 @@
+const site = 'https://визовый-центр.москва'; // change this to reflect your domain
+const pages = [
+    '',
+    'about',
+    'contacts',
+    'sitemap.xml',
+    'services/delivery',
+    'services/pasport',
+    'thanks',
+    'visas/bolgaria',
+    'visas/canada',
+    'visas/china',
+    'visas/cipr',
+    'visas/france',
+    'visas/germany',
+    'visas/greece',
+    'visas/india',
+    'visas/indonesia',
+    'visas/italy',
+    'visas/singapur',
+    'visas/spain',
+    'visas/uk',
+    'visas/usa',
+    'vnzh/greece',
+    'vnzh/israel',
+    'visas/spain',
+]
+
+export async function GET({ url }) {
+	const body = sitemap(pages);
+	const response = new Response(body);
+	response.headers.set('Cache-Control', 'max-age=0, s-maxage=3600');
+	response.headers.set('Content-Type', 'application/xml');
+	return response;
+}
+
+const sitemap = (pages) => `<?xml version="1.0" encoding="UTF-8" ?>
+<urlset
+  xmlns="https://www.sitemaps.org/schemas/sitemap/0.9"
+  xmlns:news="https://www.google.com/schemas/sitemap-news/0.9"
+  xmlns:xhtml="https://www.w3.org/1999/xhtml"
+  xmlns:mobile="https://www.google.com/schemas/sitemap-mobile/1.0"
+  xmlns:image="https://www.google.com/schemas/sitemap-image/1.1"
+  xmlns:video="https://www.google.com/schemas/sitemap-video/1.1"
+>
+  ${pages
+	.map(
+		(page) => `
+  <url>
+    <loc>${site}/${page}</loc>
+    <changefreq>daily</changefreq>
+    <priority>0.5</priority>
+  </url>
+  `
+	)
+	.join('')}
+</urlset>`;
